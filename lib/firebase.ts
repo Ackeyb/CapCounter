f465538c6c549a0c80076915d18363961fb986ed
Ackey <ackeyb@gmail.com>
2025-11-11 00:45:17 +0900

    setup safe Firebase build for Vercel

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,7 +11,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-// 初期化（重複防止）
+// Firebase 初期化（重複防止）
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const db = getFirestore(app);
-
